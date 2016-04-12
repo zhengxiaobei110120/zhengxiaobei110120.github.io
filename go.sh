22d3acc25ee3f00202e45a6e5a32f2dc93f73ddb
zhengxiaobei110120 <2759416947@qq.com>
2016-04-12 18:06:34 +0800

    新增上传脚本

---
+++ go.sh
@@ -0,0 +1,7 @@
+git config --global user.email "2759416947@qq.com"
+git config --global user.name "zhengxiaobei110120"
+git config --global push.default simple
+git add  .
+git commit -m $1
+git push
+start http://zhengxiaobei110120.github.io
